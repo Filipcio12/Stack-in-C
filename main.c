9153ed4c0d7178a2c82e8f959be275dcfce7a530
Filipcio12 <121309536+Filipcio12@users.noreply.github.com>
2023-07-05 10:42:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -27,3 +27,25 @@ void init(Stack* s)
     s->size = 0;
     s->data = 0;
 }
+
+void destroy(Stack* s) 
+{
+    free(s->data);
+}
+
+void push(Stack* s, int element)
+{
+    if (s->top >= s->size) {
+        size_t newSize = (s->size + 1) * 2;
+        int* newData = realloc(s->data, newSize * sizeof(int));
+        if (newData) {
+            s->data = newData;
+        }
+        else {
+            free(s->data);
+            abort();
+        }
+        s->size = newSize;
+    }
+    s->data[top++] = element;
+}
